b97b33add8aa0da2b8375c2be4e7c4d5a9675b76
yasminkrmn <gayanekaraman@gmail.com>
2024-03-14 12:38:36 +0100

    adding

--- 5_Transformations/Map.py
+++ 5_Transformations/Map.py
@@ -22,4 +22,23 @@ data2.take(5)
 
 # Calculating the Total Canceled Sales Amount
 
+def canceled_amount(x):
+    canceled_invoice = True if  (x.split(";")[0].startswith("C")) else False
+    quantity = float(x.split(";")[3].strip(";"))
+    price = float(x.split(";")[-3].strip(";").replace(",", "."))
+    total = quantity * price
+    return (canceled_invoice, total)
 
+canceled_data = data2.map(lambda x : canceled_amount(x))
+canceled_data.take(5)
+canceled_total = canceled_data.reduceByKey(lambda x, y: x+y)
+canceled_total.take(5)
+# [(False, 10644560.424000263), (True, -896812.4900000116)]
+
+canceled_total = canceled_total.filter(lambda x : x[0] == True)
+canceled_total.take(5)
+# [(True, -896812.4900000116)]
+
+# Calculation in sequential order
+canceled_total = canceled_data.reduceByKey(lambda x, y: x+y).filter(lambda x : x[0] == True)
+# [(True, -896812.4900000116)]
